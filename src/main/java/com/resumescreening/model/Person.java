192473b70162a359722cc34b359bb86f5edf559c
YeisraelD <Yeisraeld@gmail.com>
2025-12-19 08:19:37 +0300

    base models for the system , or base classes

---
+++ src/main/java/com/resumescreening/model/Person.java
@@ -0,0 +1,40 @@
+package com.resumescreening.model;
+
+
+public abstract class Person {
+    private String name;
+    private String email;
+    private String phone;
+
+    public Person (String name, String email, String phone){
+        this.name = name;
+        this.email =email;
+        this.phone = phone;
+    }
+
+    public abstract double calculateScore();
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name =name;
+    }
+
+    public String getPhone (){
+        return phone;
+    }
+
+    public void setPhone (String phone){
+        this.phone =phone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
